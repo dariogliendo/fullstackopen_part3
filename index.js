44ee9bfe6633ce4c4d0ebe371aa8212c9a1aa6fe
dliendo <dario.liendo@gmail.com>
2023-10-12 18:20:59 -0300

    3.8

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express')
+const morgan = require('morgan')
+
+morgan.token('body', (req) => {
+  if (req.method === 'POST') return JSON.stringify(req.body)
+  return ''
+})
 
 const app = express()
 app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 const PORT = 8001
 
 let data = [
