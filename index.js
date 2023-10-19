18a951e40c0a8a7a0c5967fa4db3607131921b79
dliendo <dario.liendo@gmail.com>
2023-10-18 21:54:22 -0300

    ex 3.15

--- index.js
+++ index.js
@@ -20,29 +20,6 @@ app.use(cors())
 app.use(express.static('dist'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let data = [
-  {
-    "id": 1,
-    "name": "Arto Hellas",
-    "number": "040-123456"
-  },
-  {
-    "id": 2,
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523"
-  },
-  {
-    "id": 3,
-    "name": "Dan Abramov",
-    "number": "12-43-234345"
-  },
-  {
-    "id": 4,
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122"
-  }
-]
-
 app.get('/api/persons', (req, res) => {
   const filter = req.query || {}
   Person.find(filter)
